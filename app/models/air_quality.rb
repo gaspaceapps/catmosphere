6bc14aa627bf26b899a9241ffc1557875f98cf9d
Brooks Swinnerton <bswinnerton@gmail.com>
2014-04-12 18:34:09 -0400

    Refining AirQuality.get

--- app/models/air_quality.rb
+++ app/models/air_quality.rb
@@ -1,27 +1,28 @@
 class AirQuality < ActiveRecord::Base
   def self.get(zipcode)
-    found = self.find_by(zipcode: zipcode)
-    if found
-      found
+    cached_air_quality = self.find_by(zipcode: zipcode)
+    if cached_air_quality
+      cached_air_quality
     else
-      endpoint = self.compile_endpoint(zipcode)
-      response = RestClient.get(endpoint)
-      parsed_response = JSON.parse(response)
-
-      air_quality_response = {
-          zipcode: zipcode,
-          aqi: parsed_response.first['AQI'],
-          category_number: parsed_response.first['Category']['Number'],
-          category_name: parsed_response.first['Category']['Name']
-      }
+      endpoint = self.compile_today_endpoint(zipcode)
+      parsed_response = JSON.parse(RestClient.get(endpoint))
+      format_air_quality_hash(parsed_response, zipcode)
       self.cacheify(air_quality_response)
     end
   end
 
   private
 
-  def self.cacheify(air_quality_response)
+  def self.format_air_quality(response, zipcode)
+    {
+        zipcode: zipcode,
+        aqi: response.first['AQI'],
+        category_number: response.first['Category']['Number'],
+        category_name: response.first['Category']['Name']
+    }
+  end
 
+  def self.cacheify(air_quality_response)
     self.find_or_create_by(zipcode: air_quality_response[:zipcode] ) do |air_quality|
       air_quality.aqi = air_quality_response[:aqi]
       air_quality.category_number = air_quality_response[:category_number]
@@ -29,7 +30,7 @@ class AirQuality < ActiveRecord::Base
     end
   end
 
-  def self.compile_endpoint(zipcode)
+  def self.compile_today_endpoint(zipcode)
     "http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=#{zipcode}&distance=25&API_KEY=408731F5-9C4F-4791-A3B9-E5BA5EE0F591"
   end
 end
