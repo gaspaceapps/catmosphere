ff96e3a07d91d2d5785e6f95a2f9a67b6fa78940
Justin Tanacredi <Jtanacredi@gmail.com>
2014-04-12 14:13:56 -0400

    Create location model

---
+++ db/migrate/20140412181339_create_locations.rb
@@ -0,0 +1,11 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.integer :lat
+      t.integer :long
+      t.integer :categoryNumber
+
+      t.timestamps
+    end
+  end
+end
