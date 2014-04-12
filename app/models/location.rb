3fd3bd6959b5c82976436722a07dfe2bdbafb9bd
Justin Tanacredi <Jtanacredi@gmail.com>
2014-04-12 15:14:26 -0400

    pasrse AirNow json

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,13 @@
 class Location < ActiveRecord::Base
+
+  def initialize
   @endpoint = "http://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude=#{lat}&longitude=-#{long}&distance=25&API_KEY=408731F5-9C4F-4791-A3B9-E5BA5EE0F591"
+  end
+
+  response = RestClient.get(endpoint)
+  parsed_data = JSON.parse(response)
+
+  aqi = parsed_data.first['AQI']
+
+
 end
