55327cf29b59b6f5b82dcf9656b92b9d2a96c581
Justin Tanacredi <Jtanacredi@gmail.com>
2014-04-12 14:18:45 -0400

    create locations in schema

--- db/migrate/20140412181339_create_locations.rb
+++ db/migrate/20140412181339_create_locations.rb
@@ -3,6 +3,7 @@ class CreateLocations < ActiveRecord::Migration
     create_table :locations do |t|
       t.integer :lat
       t.integer :long
+      t.integer :zipcode
       t.integer :categoryNumber
 
       t.timestamps
