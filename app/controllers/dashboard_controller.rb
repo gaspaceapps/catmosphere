c5c09cf44b6736ec3540f906d9617002f682a377
Brooks Swinnerton <bswinnerton@gmail.com>
2014-04-12 17:37:58 -0400

    Removing Location persistence

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   def index
     if cookies[:lat_long]
       current_location = Location.parse_from_cookie(cookies[:lat_long])
-      @air_quality = AirQuality.get(current_location.zipcode)
+      @air_quality = AirQuality.get(current_location[:zipcode])
     end
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,17 +1,12 @@
 class Location < ActiveRecord::Base
   def self.parse_from_cookie(cookie)
     split_cookie = cookie.split('|')
-    parsed_cookie = { latitude: split_cookie.first, longitude: split_cookie.last }
-    self.cacheify(parsed_cookie)
+    parsed_cookie = { latitude: split_cookie.first.to_f, longitude: split_cookie.last.to_f }
+    zipcode = self.get_zipcode(parsed_cookie[:latitude], parsed_cookie[:longitude])
+    parsed_cookie.merge({zipcode: zipcode.to_i})
   end
 
-  def self.cacheify(cookie)
-    zipcode = self.get_zipcode(cookie[:latitude], cookie[:longitude])
-    self.find_or_create_by(zipcode: zipcode) do |location|
-      location.latitude = cookie[:latitude]
-      location.longitude = cookie[:longitude]
-    end
-  end
+  private
 
   def self.get_zipcode(latitude, longitude)
     Geocoder.search("#{latitude},#{longitude}").first.data['address_components'].last['long_name']
